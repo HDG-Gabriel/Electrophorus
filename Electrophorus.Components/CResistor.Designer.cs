da6e03871bbee6d96cc7941323c7c9b2680ac27b
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-05-24 22:45:25 -0400

    Resistor componente
    Controle personalizado que representa um resistor.

---
+++ Electrophorus.Components/CResistor.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Electrophorus.Components
+{
+    partial class CResistor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CResistor));
+            this.lblResistencia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResistencia
+            // 
+            this.lblResistencia.BackColor = System.Drawing.Color.Transparent;
+            this.lblResistencia.Image = ((System.Drawing.Image)(resources.GetObject("lblResistencia.Image")));
+            this.lblResistencia.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblResistencia.Location = new System.Drawing.Point(0, 0);
+            this.lblResistencia.Name = "lblResistencia";
+            this.lblResistencia.Padding = new System.Windows.Forms.Padding(0, 8, 0, 5);
+            this.lblResistencia.Size = new System.Drawing.Size(64, 64);
+            this.lblResistencia.TabIndex = 1;
+            this.lblResistencia.Text = "1 Ω";
+            this.lblResistencia.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CResistor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lblResistencia);
+            this.Name = "CResistor";
+            this.Size = new System.Drawing.Size(64, 64);
+            this.Load += new System.EventHandler(this.CResistor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblResistencia;
+    }
+}
---
+++ Electrophorus.Components/CResistor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Electrophorus.Components
+{
+    public partial class CResistor : UserControl
+    {
+        private bool canMove;
+        public Resistor Resistor { get; }
+        public string LabelResistencia
+        {
+            get => Resistor.Resistencia.ToString();
+            set
+            {
+                Resistor.Resistencia = int.Parse(value);
+                lblResistencia.Text = $"{value} Ω";
+            }
+        }
+
+        public CResistor()
+        {
+            InitializeComponent();
+
+            Resistor = new Resistor(1);
+        }
+
+        private void CResistor_Load(object sender, EventArgs e)
+        {
+            canMove = false;
+            lblResistencia.Cursor = Cursors.SizeAll;
+
+            lblResistencia.MouseDown += CResistor_MouseDown;
+
+            lblResistencia.MouseUp += CResistor_MouseUp;
+
+            lblResistencia.MouseMove += CResistor_MouseMove;
+        }
+
+        private void CResistor_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canMove && e.Button == MouseButtons.Left)
+            {
+                Location = new Point(e.X + Location.X - Width / 2, e.Y + Location.Y - Height / 2);
+            }
+        }
+
+        private void CResistor_MouseUp(object sender, MouseEventArgs e)
+        {
+            canMove = false;
+            lblResistencia.Cursor = Cursors.SizeAll;
+        }
+
+        private void CResistor_MouseDown(object sender, MouseEventArgs e)
+        {
+            canMove = true;
+            lblResistencia.Cursor = Cursors.Default;
+        }
+    }
+}
