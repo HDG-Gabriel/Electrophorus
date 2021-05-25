b3a601b97a63596439a9d33043c8f1cee54142cf
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-05-24 22:45:31 -0400

    Create Resistor.cs

---
+++ Electrophorus.Components/Resistor.cs
@@ -0,0 +1,27 @@
+﻿namespace Electrophorus.Components
+{
+    public class Resistor
+    {
+        public int Resistencia { get; set; }
+        public double Corrente { get; set; }
+        public double Tensao { get; set; }
+        public double Potencia { get; set; }
+
+        public Resistor(int resistencia) => Resistencia = resistencia;
+
+        private void CalcularTensao(bool usarLeiDeOhm = true)
+        {
+            Tensao = Resistencia * Corrente;
+        }
+
+        private void CalcularCorrente()
+        {
+            Corrente = Tensao / Resistencia;
+        }
+
+        private void CalcularPotencia()
+        {
+            Potencia = Resistencia * Corrente;
+        }
+    }
+}
