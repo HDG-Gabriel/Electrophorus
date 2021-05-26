cfc99466b1c9a97983cd2fb0c07059e0f2a7468b
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-05-26 15:06:35 -0400

    Controle da Fonte de Tensão criado.

---
+++ Electrophorus.Components/CSource.Designer.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Electrophorus.Components
+{
+    partial class CSource
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CSource));
+            this.SuspendLayout();
+            // 
+            // lblValor
+            // 
+            this.lblValor.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.lblValor.Image = ((System.Drawing.Image)(resources.GetObject("lblValor.Image")));
+            this.lblValor.Margin = new System.Windows.Forms.Padding(0);
+            this.lblValor.Size = new System.Drawing.Size(48, 80);
+            // 
+            // CSource
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.Name = "CSource";
+            this.Size = new System.Drawing.Size(64, 96);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Electrophorus.Components/CSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace Electrophorus.Components
+{
+    public partial class CSource : CircuitComponent
+    {
+        public Source Source { get; set; }
+        public CSource()
+        {
+            InitializeComponent();
+
+            Source = new Source(10);
+            _displacementY = -13;
+        }
+
+        protected override void CircuitComponent_MouseUp(object sender, MouseEventArgs e)
+        {
+            base.CircuitComponent_MouseUp(sender, e);
+            Location = new System.Drawing.Point(Location.X, Location.Y + 4);
+        }
+    }
+}
