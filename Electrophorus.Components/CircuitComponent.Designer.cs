cc7b350295579234048b6be9cd71cff0367a1714
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-05-26 08:49:20 -0400

    Classe Circuit Component
    Classe base de onde todos os componentes que que podem ser movidos pelo usuário serão herdados.

---
+++ Electrophorus.Components/CircuitComponent.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Electrophorus.Components
+{
+    partial class CircuitComponent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CircuitComponent));
+            this.lblResistencia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResistencia
+            // 
+            this.lblResistencia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblResistencia.Image = ((System.Drawing.Image)(resources.GetObject("lblResistencia.Image")));
+            this.lblResistencia.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblResistencia.Location = new System.Drawing.Point(0, 0);
+            this.lblResistencia.Name = "lblResistencia";
+            this.lblResistencia.Padding = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.lblResistencia.Size = new System.Drawing.Size(64, 96);
+            this.lblResistencia.TabIndex = 0;
+            this.lblResistencia.Text = "1 U";
+            this.lblResistencia.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CircuitComponent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblResistencia);
+            this.Name = "CircuitComponent";
+            this.Size = new System.Drawing.Size(64, 92);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Label lblResistencia;
+    }
+}
---
+++ Electrophorus.Components/CircuitComponent.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Electrophorus.Components
+{
+    public partial class CircuitComponent : UserControl
+    {
+        private bool canMove;
+        public CircuitComponent()
+        {
+            InitializeComponent();
+
+            Load += CircuitComponent_Load;
+        }
+
+        private void CircuitComponent_Load(object sender, System.EventArgs e)
+        {
+            Size = lblResistencia.Size;
+            canMove = false;
+            lblResistencia.Cursor = Cursors.SizeAll;
+
+            lblResistencia.MouseDown += CircuitComponent_MouseDown;
+
+            lblResistencia.MouseUp += CircuitComponent_MouseUp;
+
+            lblResistencia.MouseMove += CircuitComponent_MouseMove;
+        }
+
+        private void CircuitComponent_MouseUp(object sender, MouseEventArgs e)
+        {
+            canMove = false;
+            lblResistencia.Cursor = Cursors.SizeAll;
+        }
+
+        private void CircuitComponent_MouseDown(object sender, MouseEventArgs e)
+        {
+            canMove = true;
+            lblResistencia.Cursor = Cursors.Default;
+        }
+
+        private void CircuitComponent_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canMove && e.Button == MouseButtons.Left)
+            {
+                Location = new Point(e.X + Location.X - Width / 2, e.Y + Location.Y - Height / 2);
+            }
+        }
+    }
+}
