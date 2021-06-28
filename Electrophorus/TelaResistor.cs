1cff74538ffd9ba9aabbe21cad533fcc1567edf1
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-06-28 18:13:17 -0400

    Update TelaResistor.cs
    Criado método para selecionar qual cor o usuário selecionou e retornar essa cor.

--- Electrophorus/TelaResistor.cs
+++ Electrophorus/TelaResistor.cs
@@ -14,71 +14,138 @@ namespace Electrophorus
     {
 
         public Form TelaResistor1 { get; set; }
+
         public TelaResistor()
         {
             InitializeComponent();
             BtnBackTelaInicial.Cursor = Cursors.Hand;
-
         }
-        public Color cor;
+
         private void BtnBackTelaInicial_Click(object sender, EventArgs e)
         {
             this.Hide();
             TelaResistor1.Show();
         }
-        // Cores da Faixa 1
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var NomeCor = CbFaixa1.SelectedItem.ToString();
-            var a = ImgResistor.CreateGraphics();
 
-            if (NomeCor == "Preto")
+        //Esse método acessa a caixa de cores da tela e verifica qual a cor selecionada e depois retorna a cor escolhida ;D
+        private Color CorEscolhida(ComboBox caixa)
+        {
+            var nomeCor = caixa.SelectedItem.ToString();
+            Color cor;
+
+            if (nomeCor == "Preto")
             {
                 cor = Color.Black;
             }
-            else if (NomeCor == "Marrom")
+            else if (nomeCor == "Marrom")
             {
                 cor = Color.Brown;
             }
-            else if (NomeCor == "Vermelho")
+            else if (nomeCor == "Vermelho")
             {
                 cor = Color.Red;
             }
-            else if (NomeCor == "Laranja")
+            else if (nomeCor == "Laranja")
             {
                 cor = Color.Orange;
             }
-            else if (NomeCor == "Amarelo")
+            else if (nomeCor == "Amarelo")
             {
                 cor = Color.Yellow;
             }
-            else if (NomeCor == "Verde")
+            else if (nomeCor == "Verde")
             {
                 cor = Color.Green;
             }
-            else if (NomeCor == "Azul")
+            else if (nomeCor == "Azul")
             {
                 cor = Color.Blue;
             }
-            else if (NomeCor == "Violeta")
+            else if (nomeCor == "Violeta")
             {
                 cor = Color.Violet;
             }
-            else if (NomeCor == "Cinza")
+            else if (nomeCor == "Cinza")
             {
                 cor = Color.Gray;
             }
-            else if(NomeCor == "Branco")
+            else
             {
                 cor = Color.White;
             }
+
+            return cor;
+        }
+
+        // Cores da Faixa 1
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var a = ImgResistor.CreateGraphics();
+
+            var cor = CorEscolhida(CbFaixa1);
+            
             var brush = new SolidBrush(cor);
 
             a.FillRectangle(brush, 104, 5, 24, 160);
             a.Dispose();
-
-            
         }
+
+        // Cores Faixa 2
+        private void CbFaixa2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var NomeCor = CbFaixa2.SelectedItem.ToString();
+            var a = ImgResistor.CreateGraphics();
+
+            var cor = CorEscolhida(CbFaixa2);
+            
+            var brush = new SolidBrush(cor);
+
+            a.FillRectangle(brush, 168, 29, 32, 112);
+            a.Dispose();
+        }
+
+        // Cores Faixa 3
+        private void CbFaixa3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var NomeCor = CbFaixa3.SelectedItem.ToString();
+            var a = ImgResistor.CreateGraphics();
+
+            var cor = CorEscolhida(CbFaixa3);
+
+            var brush = new SolidBrush(cor);
+
+            a.FillRectangle(brush, 240, 29, 32, 112);
+            a.Dispose();
+        }
+
+        // Cores Faixa 4
+        private void CbFaixa4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var NomeCor = CbFaixa4.SelectedItem.ToString();
+            var a = ImgResistor.CreateGraphics();
+
+            var cor = CorEscolhida(CbFaixa4);
+
+            var brush = new SolidBrush(cor);
+
+            a.FillRectangle(brush, 312, 29, 32, 112);
+            a.Dispose();
+        }
+
+        // Cores Faixa 5
+        private void CbFaixa5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var NomeCor = CbFaixa5.SelectedItem.ToString();
+            var a = ImgResistor.CreateGraphics();
+
+            var cor = CorEscolhida(CbFaixa5);
+
+            var brush = new SolidBrush(cor);
+
+            a.FillRectangle(brush, 384, 5, 24, 160);
+            a.Dispose();
+        }
+
         // Botão de Reset
         private void BtnReset_Click(object sender, EventArgs e)
         {
@@ -90,210 +157,6 @@ namespace Electrophorus
             CbFaixa5.ResetText();
             CbValorResistor.ResetText();
 
-        }
-        // Cores Faixa 2
-        private void CbFaixa2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var NomeCor = CbFaixa2.SelectedItem.ToString();
-            var a = ImgResistor.CreateGraphics();
-
-            if (NomeCor == "Preto")
-            {
-                cor = Color.Black;
-            }
-            else if (NomeCor == "Marrom")
-            {
-                cor = Color.Brown;
-            }
-            else if (NomeCor == "Vermelho")
-            {
-                cor = Color.Red;
-            }
-            else if (NomeCor == "Laranja")
-            {
-                cor = Color.Orange;
-            }
-            else if (NomeCor == "Amarelo")
-            {
-                cor = Color.Yellow;
-            }
-            else if (NomeCor == "Verde")
-            {
-                cor = Color.Green;
-            }
-            else if (NomeCor == "Azul")
-            {
-                cor = Color.Blue;
-            }
-            else if (NomeCor == "Violeta")
-            {
-                cor = Color.Violet;
-            }
-            else if (NomeCor == "Cinza")
-            {
-                cor = Color.Gray;
-            }
-            else if (NomeCor == "Branco")
-            {
-                cor = Color.White;
-            }
-            var brush = new SolidBrush(cor);
-
-            a.FillRectangle(brush, 168, 29, 32, 112);
-            a.Dispose();
-        }
-        // Cores Faixa 3
-        private void CbFaixa3_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var NomeCor = CbFaixa3.SelectedItem.ToString();
-            var a = ImgResistor.CreateGraphics();
-
-            if (NomeCor == "Preto")
-            {
-                cor = Color.Black;
-            }
-            else if (NomeCor == "Marrom")
-            {
-                cor = Color.Brown;
-            }
-            else if (NomeCor == "Vermelho")
-            {
-                cor = Color.Red;
-            }
-            else if (NomeCor == "Laranja")
-            {
-                cor = Color.Orange;
-            }
-            else if (NomeCor == "Amarelo")
-            {
-                cor = Color.Yellow;
-            }
-            else if (NomeCor == "Verde")
-            {
-                cor = Color.Green;
-            }
-            else if (NomeCor == "Azul")
-            {
-                cor = Color.Blue;
-            }
-            else if (NomeCor == "Violeta")
-            {
-                cor = Color.Violet;
-            }
-            else if (NomeCor == "Cinza")
-            {
-                cor = Color.Gray;
-            }
-            else if (NomeCor == "Branco")
-            {
-                cor = Color.White;
-            }
-            var brush = new SolidBrush(cor);
-
-            a.FillRectangle(brush, 240, 29, 32, 112);
-            a.Dispose();
-        }
-        // Cores Faixa 4
-        private void CbFaixa4_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var NomeCor = CbFaixa4.SelectedItem.ToString();
-            var a = ImgResistor.CreateGraphics();
-
-            if (NomeCor == "Preto")
-            {
-                cor = Color.Black;
-            }
-            else if (NomeCor == "Marrom")
-            {
-                cor = Color.Brown;
-            }
-            else if (NomeCor == "Vermelho")
-            {
-                cor = Color.Red;
-            }
-            else if (NomeCor == "Laranja")
-            {
-                cor = Color.Orange;
-            }
-            else if (NomeCor == "Amarelo")
-            {
-                cor = Color.Yellow;
-            }
-            else if (NomeCor == "Verde")
-            {
-                cor = Color.Green;
-            }
-            else if (NomeCor == "Azul")
-            {
-                cor = Color.Blue;
-            }
-            else if (NomeCor == "Violeta")
-            {
-                cor = Color.Violet;
-            }
-            else if (NomeCor == "Cinza")
-            {
-                cor = Color.Gray;
-            }
-            else if (NomeCor == "Branco")
-            {
-                cor = Color.White;
-            }
-            else if (NomeCor == "Ouro")
-            {
-                cor = Color.Gold;
-            }
-            else if (NomeCor == "Prata")
-            {
-                cor = Color.Silver;
-            }
-            var brush = new SolidBrush(cor);
-
-            a.FillRectangle(brush, 312, 29, 32, 112);
-            a.Dispose();
-        }
-        // Cores Faixa 5
-        private void CbFaixa5_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var NomeCor = CbFaixa5.SelectedItem.ToString();
-            var a = ImgResistor.CreateGraphics();
-
-            if (NomeCor == "Marrom")
-            {
-                cor = Color.Brown;
-            }
-            else if (NomeCor == "Vermelho")
-            {
-                cor = Color.Red;
-            }
-            else if (NomeCor == "Verde")
-            {
-                cor = Color.Green;
-            }
-            else if (NomeCor == "Azul")
-            {
-                cor = Color.Blue;
-            }
-            else if (NomeCor == "Violeta")
-            {
-                cor = Color.Violet;
-            }
-            else if (NomeCor == "Cinza")
-            {
-                cor = Color.Gray;
-            }
-            else if (NomeCor == "Ouro")
-            {
-                cor = Color.Gold;
-            }
-            else if (NomeCor == "Prata")
-            {
-                cor = Color.Silver;
-            }
-            var brush = new SolidBrush(cor);
-
-            a.FillRectangle(brush, 384, 5, 24, 160);
-            a.Dispose();
         }
     }
 }
