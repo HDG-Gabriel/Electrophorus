b26fea4b252460ebd600cbda1100157fb0800aa1
Gabriel Bento da Silva <67703889+HDG-Gabriel@users.noreply.github.com>
2021-10-20 18:29:06 -0300

    Create Node.cs

---
+++ Electrophorus.Rendering/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SkiaSharp;
+using SkiaSharp.Views;
+
+namespace Electrophorus.Rendering
+{
+    public class Node
+    {
+        // All elements will have the same radius
+        public static float Radius { get; set; } = 6;
+        public SKColor Color { get; set; } = SKColors.Red;
+        public SKPoint Location {  get; set; } = new SKPoint();
+        public SKPaint Paint { get; }
+
+        public Node() => Paint = new SKPaint() { Color = Color};
+
+        public static bool IsInside(MouseEventArgs e, Node n)
+        {
+            return e.X >= n.Location.X - Radius && e.X <= n.Location.X + Radius &&
+                e.Y >= n.Location.Y - Radius && e.Y <= n.Location.Y + Radius;
+        }
+    }
+}
