d8c4c3ef9c2f6e7c0196dfb639fb5526c4db500e
Gabriel Bento da Silva <67703889+HDG-Gabriel@users.noreply.github.com>
2021-10-20 18:41:51 -0300

    Update Wire.cs
    Expande o fio horizontalmente para a direita

--- Electrophorus.Rendering/Wire.cs
+++ Electrophorus.Rendering/Wire.cs
@@ -1,4 +1,5 @@
 ﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,8 +12,18 @@ namespace Electrophorus.Rendering
 {
     public class Wire : ICircuitComponent
     {
-        public SKPoint Location { get; set; }
-        public SKRect Size { get; set; }
+        private SKPoint _location;
+        public SKPoint Location
+        {
+            get => _location;
+            set
+            {
+                _location = value;
+                NodeIn.Location = value;
+                NodeOut.Location = new SKPoint(value.X + Size.Width, value.Y);
+            }
+        }
+        public SKRect Size {get; set; }
         public SKPaint Paint { get; set; }
         private bool _isAbove = false;
         public bool IsAbove
@@ -24,10 +35,18 @@ namespace Electrophorus.Rendering
                 Paint.Color = _isAbove ? SKColors.Blue : SKColors.Gray;
             }
         }
+        public Node NodeIn { get; set; }
+        public Node NodeOut { get; set; }
+        public bool CanGrowUp { get; set; } = false;
+        public bool CanMove { get; set; } = false;
 
         public Wire()
         {
+            NodeIn = new Node();
+            NodeOut = new Node() { Location = new SKPoint(Location.X + Board.CellSize, Location.Y)};
+
             Size = new SKRect(0, 0, Board.CellSize, Board.CellSize);
+
             Paint = new SKPaint()
             {
                 Color = SKColors.Gray,
@@ -36,5 +55,23 @@ namespace Electrophorus.Rendering
         }
 
         public bool IsInside(MouseEventArgs e) => (e.X >= Location.X && e.X <= Location.X + Size.Width && e.Y >= Location.Y && e.Y <= Location.Y + Size.Height);
+
+        public void Draw(SKCanvas canvas)
+        {
+            canvas.DrawRect(Location.X, Location.Y, Size.Width, Size.Height, Paint);
+            canvas.DrawCircle(NodeIn.Location, Node.Radius, NodeIn.Paint);
+            canvas.DrawCircle(NodeOut.Location, Node.Radius, NodeOut.Paint);
+        }
+
+        public void GrowUp(SKControl view, MouseEventArgs e)
+        {
+            var width = ((int)(e.X - Location.X) / Board.CellSize) * Board.CellSize;
+            Debug.WriteLine(width);
+            if (width < Board.CellSize) return;
+
+            Size = new SKRect(0, 0, width, Size.Height);
+            NodeOut.Location = new SKPoint(Location.X + Size.Width, NodeOut.Location.Y);
+            view.Refresh();
+        }
     }
 }
