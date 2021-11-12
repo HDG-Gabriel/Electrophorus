5bfff508ac65184e04da8e04832c7adeea47900c
JOAO-VFG <78110240+JOAO-VFG@users.noreply.github.com>
2021-11-12 14:50:09 -0300

    att componentes

--- Electrophorus/Componente.Designer.cs
+++ Electrophorus/Componente.Designer.cs
@@ -34,8 +34,14 @@ namespace Electrophorus
             this.button3 = new System.Windows.Forms.Button();
             this.Bateria = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,7 +57,7 @@ namespace Electrophorus
             this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(3, 87);
+            this.button2.Location = new System.Drawing.Point(11, 254);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 27);
             this.button2.TabIndex = 1;
@@ -70,7 +76,7 @@ namespace Electrophorus
             this.button3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(118, 87);
+            this.button3.Location = new System.Drawing.Point(132, 254);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(105, 27);
             this.button3.TabIndex = 2;
@@ -82,7 +88,7 @@ namespace Electrophorus
             this.Bateria.AutoSize = true;
             this.Bateria.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Bateria.ForeColor = System.Drawing.Color.White;
-            this.Bateria.Location = new System.Drawing.Point(3, 0);
+            this.Bateria.Location = new System.Drawing.Point(3, 15);
             this.Bateria.Name = "Bateria";
             this.Bateria.Size = new System.Drawing.Size(70, 25);
             this.Bateria.TabIndex = 0;
@@ -91,33 +97,123 @@ namespace Electrophorus
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.Bateria);
             this.panel1.Location = new System.Drawing.Point(11, 18);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(226, 117);
+            this.panel1.Size = new System.Drawing.Size(226, 230);
             this.panel1.TabIndex = 4;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(3, 43);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 23);
-            this.textBox1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(194, 45);
+            this.label1.Location = new System.Drawing.Point(182, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 21);
             this.label1.TabIndex = 4;
             this.label1.Text = "(Ω)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Corrente Elétrica";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 25);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Tensão";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Selecione uma cor",
+            "Preto",
+            "Marrom",
+            "Vermelho",
+            "Laranja",
+            "Amarelo",
+            "Verde",
+            "Azul",
+            "Violeta",
+            "Cinza",
+            "Branco"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 195);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 31);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Selecione uma cor",
+            "Preto",
+            "Marrom",
+            "Vermelho",
+            "Laranja",
+            "Amarelo",
+            "Verde",
+            "Azul",
+            "Violeta",
+            "Cinza",
+            "Branco"});
+            this.comboBox2.Location = new System.Drawing.Point(3, 117);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(173, 31);
+            this.comboBox2.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(179, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 21);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "(mA)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(179, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 21);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "(V)";
             // 
             // Componente
             // 
@@ -125,8 +221,10 @@ namespace Electrophorus
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
             this.Name = "Componente";
-            this.Size = new System.Drawing.Size(254, 148);
+            this.Size = new System.Drawing.Size(254, 284);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -140,5 +238,11 @@ namespace Electrophorus
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Electrophorus/componenteBateria.Designer.cs
+++ Electrophorus/componenteBateria.Designer.cs
@@ -34,22 +34,26 @@ namespace Electrophorus
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Bateria = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.CbFaixa2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.CbFaixa2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.Bateria);
             this.panel1.Location = new System.Drawing.Point(9, 17);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(232, 115);
+            this.panel1.Size = new System.Drawing.Size(232, 161);
             this.panel1.TabIndex = 5;
             // 
             // label1
@@ -67,7 +71,7 @@ namespace Electrophorus
             // 
             this.textBox1.Location = new System.Drawing.Point(3, 43);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 23);
+            this.textBox1.Size = new System.Drawing.Size(179, 23);
             this.textBox1.TabIndex = 3;
             // 
             // Bateria
@@ -75,30 +79,11 @@ namespace Electrophorus
             this.Bateria.AutoSize = true;
             this.Bateria.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Bateria.ForeColor = System.Drawing.Color.White;
-            this.Bateria.Location = new System.Drawing.Point(3, 0);
+            this.Bateria.Location = new System.Drawing.Point(3, 15);
             this.Bateria.Name = "Bateria";
             this.Bateria.Size = new System.Drawing.Size(64, 25);
             this.Bateria.TabIndex = 0;
             this.Bateria.Text = "Bateria";
-            // 
-            // button3
-            // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.BackColor = System.Drawing.Color.Black;
-            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button3.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(124, 85);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(105, 27);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "OK";
-            this.button3.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -112,21 +97,85 @@ namespace Electrophorus
             this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(3, 85);
+            this.button2.Location = new System.Drawing.Point(9, 184);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 27);
             this.button2.TabIndex = 7;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = false;
             // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(136, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // CbFaixa2
+            // 
+            this.CbFaixa2.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CbFaixa2.FormattingEnabled = true;
+            this.CbFaixa2.Items.AddRange(new object[] {
+            "Selecione uma cor",
+            "Preto",
+            "Marrom",
+            "Vermelho",
+            "Laranja",
+            "Amarelo",
+            "Verde",
+            "Azul",
+            "Violeta",
+            "Cinza",
+            "Branco"});
+            this.CbFaixa2.Location = new System.Drawing.Point(3, 111);
+            this.CbFaixa2.Name = "CbFaixa2";
+            this.CbFaixa2.Size = new System.Drawing.Size(179, 31);
+            this.CbFaixa2.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Corrente Elétrica";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(185, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 21);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "(mA)";
+            // 
             // componenteBateria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
             this.Name = "componenteBateria";
-            this.Size = new System.Drawing.Size(254, 148);
+            this.Size = new System.Drawing.Size(254, 218);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -141,5 +190,8 @@ namespace Electrophorus
         private System.Windows.Forms.Label Bateria;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CbFaixa2;
     }
 }
