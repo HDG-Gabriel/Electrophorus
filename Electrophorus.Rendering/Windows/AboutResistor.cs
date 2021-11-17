e1b23e71fe41d21235ad46acd786bc46e5f37613
Gabriel Bento da Silva <67703889+HDG-Gabriel@users.noreply.github.com>
2021-11-17 17:50:37 -0300

    Janela de configuração trocada de controle para form

---
+++ Electrophorus.Rendering/Windows/AboutResistor.cs
@@ -0,0 +1,38 @@
+﻿using SharpCircuit.src;
+using SkiaSharp.Views.Desktop;
+using System;
+using System.Windows.Forms;
+using lib = SharpCircuit.src.elements;
+
+namespace Electrophorus.Rendering.Windows
+{
+    public partial class AboutResistor : Form
+    {
+        public SKControl View { get; set; }
+        private lib.Resistor _resistor;
+        public AboutResistor()
+        {
+            InitializeComponent();
+
+            imgOK.Click += ImgOK_Click;
+        }
+
+        public AboutResistor(lib.Resistor r) : this()
+        {
+            _resistor = r;
+            txtResistencia.Text = r.resistance.ToString();
+            lblCorrente.Text = SIUnits.CurrentRounded(r.getCurrent(), 3);
+            lblDDP.Text = SIUnits.Voltage(r.getVoltageDelta());
+        }
+
+        private void ImgOK_Click(object sender, System.EventArgs e)
+        {
+            if (txtResistencia.Text != string.Empty)
+            {
+                _resistor.resistance = int.Parse(txtResistencia.Text);
+            }
+            if (View != null) View.Refresh();
+            Close();
+        }
+    }
+}
---
+++ Electrophorus.Rendering/Windows/AboutSource.cs
@@ -0,0 +1,45 @@
+﻿using SharpCircuit.src;
+using SkiaSharp.Views.Desktop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using lib = SharpCircuit.src.elements.voltage;
+
+namespace Electrophorus.Rendering.Windows
+{
+    public partial class AboutSource : Form
+    {
+        private lib.DCVoltageSource _source;
+        public SKControl View { get; set; }
+
+        public AboutSource()
+        {
+            InitializeComponent();
+
+            imgOK.Click += ImgOK_Click;
+        }
+
+        private void ImgOK_Click(object sender, EventArgs e)
+        {
+            if (txtValor.Text != string.Empty)
+            {
+                _source.maxVoltage = int.Parse(txtValor.Text);
+            }
+            if (View != null) View.Refresh();
+            Close();
+        }
+
+        public AboutSource(lib.DCVoltageSource s) : this()
+        {
+            _source = s;
+            txtValor.Text = s.maxVoltage.ToString();
+            lblCorrente.Text = SIUnits.CurrentRounded(s.getCurrent(), 3);
+        }
+    }
+}
